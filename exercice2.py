38d05b358c95fe025fb501246493c2eba53d0ceb
Dina Chaouki <dinachaouki82@gmail.com>
2025-12-01 14:58:39 +0100

    modification des valeurs de l'exo

--- exercice2.py
+++ exercice2.py
@@ -1,6 +1,6 @@
-a = 20
-b = 25
-c = 30
+a = 25
+b = 15
+c = 35
 if (a >= b and a <= c) or (a >= c and a <= b):
     median = a
 elif (b >= a and b <= c) or (b >= c and b <= a):
@@ -8,4 +8,4 @@ elif (b >= a and b <= c) or (b >= c and b <= a):
 else:
     median = c
 
-print("La median est", median)
+print("La median entre les trois nombres est", median)
