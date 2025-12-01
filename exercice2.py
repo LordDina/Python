22bed82c9ea6b6d9e9d8434bd78922ebcfc1d061
Dina Chaouki <dinachaouki82@gmail.com>
2025-12-01 12:47:48 +0100

    Ajout des exercices Python

---
+++ exercice2.py
@@ -0,0 +1,13 @@
+a = int(input("entrer le premier nombre: "))
+b = int(input("entrer le deucieme nombre: "))
+c = int(input("entrer le troisieme: "))
+
+if (a >= b and a <= c) or (a >= c and a <= b):
+    median = a
+elif (b >= a and b <= c) or (b >= c and b <= a):
+    median = b
+else:
+    median = c
+
+print("La median est")
+print(median)
